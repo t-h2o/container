7c8f3073ce7df9eb049216f99c4137c9b7f50611
|°_°| <grivel.theo@protonmail.com>
2022-12-15 01:04:36 +0100

    main: update

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,9 +6,14 @@ static void	test_vector(void)
 {
 	Vector<int>	vec;
 
+	section("assign 7 times the value 100");
 	vec.assign(7, 100);
 
-	std::cout << vec;
+	section("print vec");
+	std::cout << vec << std::endl;
+
+	section("print the 3th number");
+	std::cout << vec[3] << std::endl;
 }
 
 int	main(void)
