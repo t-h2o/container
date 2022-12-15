4e09d82697e08a3599a00b1beb2baf617b93be9c
theo <grivel.theo@protonmail.com>
2022-12-15 10:31:28 +0100

    main: add the real vector

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,7 @@
 #include	<iostream>
 #include	"color.hpp"
 #include	"Vector.hpp"
+#include	<vector>
 
 static void	test_my_vector(void)
 {
@@ -16,11 +17,23 @@ static void	test_my_vector(void)
 	std::cout << vec[3] << std::endl;
 }
 
+static void	test_vector(void)
+{
+	std::vector<int>	vec;
+
+	section("assign 7 times the value 100");
+	vec.assign(11, 100);
+
+	section("print the 3th number");
+	std::cout << vec[3] << std::endl;
+}
+
 int	main(void)
 {
 	title("Main");
 
 	test_my_vector();
+	test_vector();
 
 	return 0;
 }
