cc1a5be5ca28220b5adddd4c2267eeff5e442b55
To grivel <grivel.theo@protonmail.com>
2023-02-01 04:44:05 +0100

    vector: complete typedef

--- headers/Vector.hpp
+++ headers/Vector.hpp
@@ -25,7 +25,9 @@ template <typename T, typename Alloc = std::allocator<T> > class vector
 
 	typedef typename allocator_type::pointer		 pointer;
 	typedef typename allocator_type::reference		 reference;
+	typedef typename allocator_type::const_pointer	 const_pointer;
 	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::difference_type difference_type;
 	typedef typename allocator_type::size_type		 size_type;
 
 	typedef ft::random_access_iterator<T> iterator;
