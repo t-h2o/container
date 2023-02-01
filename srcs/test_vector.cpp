092959facb394144782cc6dcfa4707fa90eb2ad1
To grivel <grivel.theo@protonmail.com>
2023-02-01 22:06:02 +0100

    test: remove gtest

--- srcs/test_vector.cpp
+++ srcs/test_vector.cpp
@@ -2,7 +2,6 @@
 #include "Iterator.hpp"
 #include "Vector.hpp"
 #include "color.hpp"
-#include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
 
@@ -21,63 +20,36 @@ template <typename T>
 static void
 expect_equal(std::vector<T> &vec_std, ft::vector<T> &vec_ft)
 {
-	EXPECT_EQ(vec_ft.size(), vec_std.size());
-	EXPECT_EQ(vec_ft.max_size(), vec_std.max_size());
-	EXPECT_EQ(vec_ft.capacity(), vec_std.capacity());
-	EXPECT_EQ(vec_ft.empty(), vec_std.empty());
-
 	T const *data_std = vec_std.data();
 	T const *data_ft = vec_ft.data();
 
 	for (size_t i = 0; i < vec_std.size(); ++i)
-		EXPECT_EQ(data_ft[i], data_std[i]);
 
-	if (vec_ft.empty())
-		return;
+		if (vec_ft.empty())
+			return;
 
 	typename std::vector<T>::iterator it_std(vec_std.begin());
 	typename ft::vector<T>::iterator  it_ft(vec_ft.begin());
 
-	EXPECT_EQ(*it_ft, *it_std);
-
 	for (size_t i = 0; i < vec_std.size(); ++i)
-		EXPECT_EQ(it_ft[i], it_std[i]);
 
-	EXPECT_EQ(vec_ft.begin() > vec_ft.end(), vec_std.begin() > vec_std.end());
-	EXPECT_EQ(vec_ft.begin() < vec_ft.end(), vec_std.begin() < vec_std.end());
-	EXPECT_EQ(vec_ft.begin() >= vec_ft.end(), vec_std.begin() >= vec_std.end());
-	EXPECT_EQ(vec_ft.begin() <= vec_ft.end(), vec_std.begin() <= vec_std.end());
-
-	EXPECT_EQ(vec_ft.end() > vec_ft.end(), vec_std.end() > vec_std.end());
-	EXPECT_EQ(vec_ft.end() < vec_ft.end(), vec_std.end() < vec_std.end());
-	EXPECT_EQ(vec_ft.end() >= vec_ft.end(), vec_std.end() >= vec_std.end());
-	EXPECT_EQ(vec_ft.end() <= vec_ft.end(), vec_std.end() <= vec_std.end());
-
-	if (vec_ft.size() < 3)
-		return;
+		if (vec_ft.size() < 3)
+			return;
 
 	it_ft = vec_ft.begin();
 	it_std = vec_std.begin();
-	EXPECT_EQ(*(it_ft++), *(it_std++));
-	EXPECT_EQ(*(++it_ft), *(++it_std));
-	EXPECT_EQ(*(it_ft--), *(it_std--));
-	EXPECT_EQ(*(--it_ft), *(--it_std));
 
 	it_ft += 1;
 	it_std += 1;
-	EXPECT_EQ(*(it_ft), *(it_std));
 
 	it_ft = it_ft + 1;
 	it_std = it_std + 1;
-	EXPECT_EQ(*(it_ft), *(it_std));
 
 	it_ft -= 1;
 	it_std -= 1;
-	EXPECT_EQ(*(it_ft), *(it_std));
 
 	it_ft = it_ft - 1;
 	it_std = it_std - 1;
-	EXPECT_EQ(*(it_ft), *(it_std));
 }
 
 template <typename T>
