59fbcb92e4c6d68d5552a913b45f234e32fe8315
|°_°| <grivel.theo@protonmail.com>
2023-02-04 17:12:23 +0100

    map: add expected prototype

--- headers/Map.hpp
+++ headers/Map.hpp
@@ -12,6 +12,8 @@
 namespace ft
 {
 
+// template < class Key,  map::key_type class T,  map::mapped_type class Compare = less<Key>,  map::key_compare class Alloc = allocator<pair<const Key,T> >  map::allocator_type > class map;
+
 template <typename T1, typename T2> class map
 {
 	enum e_side
