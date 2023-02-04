81ea88a2bd6c4f96cee74b2a8d518ef31a638fff
|°_°| <grivel.theo@protonmail.com>
2023-02-04 18:26:35 +0100

    map: typedef of allocator_type

--- headers/Map.hpp
+++ headers/Map.hpp
@@ -23,6 +23,15 @@ template <typename T1, typename T2, typename Alloc = std::allocator<ft::pair<T1,
 		RIGHT
 	};
 
+	typedef Alloc allocator_type;
+
+	typedef typename allocator_type::pointer		 pointer;
+	typedef typename allocator_type::reference		 reference;
+	typedef typename allocator_type::const_pointer	 const_pointer;
+	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::difference_type difference_type;
+	typedef typename allocator_type::size_type		 size_type;
+
 	typedef ft::pair<T1, T2> pair;
 
 	class Color
