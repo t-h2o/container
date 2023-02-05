701fde2668646324b2e732ff8c85abffc52fce79
|°_°| <grivel.theo@protonmail.com>
2023-02-05 22:52:30 +0100

    c.l: add allocator typedef

--- headers/chain_list.hpp
+++ headers/chain_list.hpp
@@ -21,6 +21,16 @@ template <typename T, typename Alloc = std::allocator<T> > class chain_list
 	typedef ft::_node<T> node;
 	typedef node		*node_ptr;
 
+	typedef Alloc allocator_type;
+
+	typedef typename allocator_type::value_type		 value_type;
+	typedef typename allocator_type::pointer		 pointer;
+	typedef typename allocator_type::reference		 reference;
+	typedef typename allocator_type::const_pointer	 const_pointer;
+	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::difference_type difference_type;
+	typedef typename allocator_type::size_type		 size_type;
+
   public:
 	chain_list(void);
 
