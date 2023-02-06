e4162e7b86c647f048e2de30693a5e98c90f4491
|°_°| <grivel.theo@protonmail.com>
2023-02-06 12:52:30 +0100

    c.l: test update

--- srcs/test_chain_list.cpp
+++ srcs/test_chain_list.cpp
@@ -1,18 +1,35 @@
+#include "Awesome.hpp"
 #include "Tdd.hpp"
 #include "color.hpp"
 #include "test.hpp"
 
 #include "chain_list.hpp"
 
+template <typename T>
+static void
+test(void)
+{
+	ft::chain_list<T> a;
+
+	std::cout << "size: " << a.size() << std::endl;
+	a.last();
+
+	a.put(42);
+	std::cout << "size: " << a.size() << std::endl;
+	a.last();
+
+	a.put(1234);
+	std::cout << "size: " << a.size() << std::endl;
+	a.last();
+}
+
 void
 test_chain_list(void)
 {
 	title("Chain list");
+	section("test int");
 
-	ft::chain_list<int> a;
-	a.last();
-	a.put(42);
-	a.last();
-	a.put(1234);
-	a.last();
+	test<int>();
+	section("test Awesome");
+	test<Awesome>();
 }
