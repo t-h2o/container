be77307335299d10e1169cb6c018e6095b01939d
|°_°| <grivel.theo@protonmail.com>
2023-02-09 19:46:23 +0100

    Tdd: add "error" as a keyword

--- templates/Tdd.cpp
+++ templates/Tdd.cpp
@@ -4,5 +4,5 @@ Tdd::expected(T const &a, T const &b)
 {
 	if (a == b)
 		return;
-	std::cout << "a: " << a << std::endl << "b: " << b << std::endl;
+	std::cout << "error:\na: " << a << std::endl << "b: " << b << std::endl;
 };
