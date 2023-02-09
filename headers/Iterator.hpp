3cf96e5c85a3ff9b4456b9f48605d775d23793b3
|°_°| <grivel.theo@protonmail.com>
2023-02-09 22:06:25 +0100

    iterator: update iterator_traits

--- headers/Iterator.hpp
+++ headers/Iterator.hpp
@@ -3,6 +3,10 @@
 
 #include <cstddef>
 
+/** Inspired by the GNU ISO C++ Library **
+ * From this file: /usr/include/c++/${VERSION}/bits/stl_iterator_base_types.h
+ */
+
 namespace ft
 {
 
@@ -76,6 +80,26 @@ template <typename _Iterator> struct iterator_traits
 	typedef typename _Iterator::difference_type	  difference_type;
 	typedef typename _Iterator::pointer			  pointer;
 	typedef typename _Iterator::reference		  reference;
+};
+
+/// Partial specialization for pointer types.
+template <typename _Tp> struct iterator_traits<_Tp *>
+{
+	typedef random_access_iterator_tag iterator_category;
+	typedef _Tp						   value_type;
+	typedef ptrdiff_t				   difference_type;
+	typedef _Tp						  *pointer;
+	typedef _Tp						  &reference;
+};
+
+/// Partial specialization for const pointer types.
+template <typename _Tp> struct iterator_traits<const _Tp *>
+{
+	typedef random_access_iterator_tag iterator_category;
+	typedef _Tp						   value_type;
+	typedef ptrdiff_t				   difference_type;
+	typedef const _Tp				  *pointer;
+	typedef const _Tp				  &reference;
 };
 
 } /* namespace ft */
