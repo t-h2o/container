6505d6f17c4d9fef254554565ec14ef60caa2941
|°_°| <grivel.theo@protonmail.com>
2023-02-14 22:08:32 +0100

    add title

--- srcs/test_vector_2.cpp
+++ srcs/test_vector_2.cpp
@@ -22,14 +22,17 @@ start_test(void)
 	ft::vector<TYPE>  vec_ft;
 
 	void (*f[2])(std::vector<TYPE> &, ft::vector<TYPE> &, TYPE);
-	f[0] = &test_push_back;
+	f[0] = &test_push_back<TYPE>;
 	f[0](vec_std, vec_ft, 0);
 }
 
 void
 test_vector_2(void)
 {
+	title("test vector 2.0");
+
 	start_test<int>();
 	start_test<Awesome>();
+
 	return;
 }
